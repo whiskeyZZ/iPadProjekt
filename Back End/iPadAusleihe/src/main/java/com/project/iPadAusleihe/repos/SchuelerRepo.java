fb6a062efd1075a91bcbddbf4b140e5955da9575
Julian <ju.o@example.com>
2023-01-31 10:48:16 +0100

    Kleinere Änderungen im Design

--- Back End/iPadAusleihe/src/main/java/com/project/iPadAusleihe/repos/SchuelerRepo.java
+++ Back End/iPadAusleihe/src/main/java/com/project/iPadAusleihe/repos/SchuelerRepo.java
@@ -12,4 +12,8 @@ public interface SchuelerRepo extends JpaRepository<Schueler, Long> {
 
     @Query(value = "SELECT * FROM schueler WHERE klasseid = 1",  nativeQuery = true)
     Collection<Schueler> findAllSchuelerInSpecificClass();
+
+    //@Query(value = "SELECT * FROM schueler WHERE klasseid = :klasse_id",  nativeQuery = true)
+    //Collection<Schueler> findAllSchuelerInSpecificClass(@Param("klasse_id)") int klasse_id);
+
 }
